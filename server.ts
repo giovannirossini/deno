c2f0fc10b64f8b04fa68c3853542189ee1d6cb85
Giovanni Rossini <giovanni.rossini@ab-inbev.com>
2022-08-25 12:01:49 -0300

    Clean up the code

--- server.ts
+++ server.ts
@@ -2,9 +2,9 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./router.ts";
 
 const app = new Application();
-const port = parseInt(Deno.env.get("PORT") ?? '3000')
+const port = parseInt(Deno.env.get('PORT') ?? '3000')
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen(":" + port);
+await app.listen(`:${port}`);
