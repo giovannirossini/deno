c6fc93ed0b1cdcfd8f3d4082ca57e281caf6601e
Giovanni Rossini <giovannijrrossini@gmail.com>
2022-08-25 10:42:59 -0300

    Adding dynamic variable to port

--- server.ts
+++ server.ts
@@ -2,8 +2,9 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./router.ts";
 
 const app = new Application();
+const port = parseInt(Deno.env.get("PORT") ?? '3000')
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen(":3000");
+await app.listen(":" + port);
